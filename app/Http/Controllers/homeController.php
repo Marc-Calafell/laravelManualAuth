697261be2e090c354f8b6dbc27a95cc7e243e320
Marc <mcalafellsmx@gmail.com>
2016-10-04 19:41:40 +0200

    Add a sqlite

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -13,14 +13,30 @@ use App\Http\Requests;
 //}
 
 
+
+
 class homeController extends Controller
 {
     function index () {
        // $data= ['username'=>'Marc'];
-        $user= User::find();
+       // $user= User::find();
 
 
         //return view('auth.home')-> withUsername($data['username']);
-        return view('auth.home')-> withUsername($user);
+       // return view('auth.home')-> withUsername($user);
+
+        $user->name = "asdasd";
+        $pdo = new PDO('sqlite:/home/marc/Codi/laravelManualAuth/database/database.sqlite');
+        $query = $pdo->prepare('SELECT * FROM users WHERE id=1');
+        $query->execute();
+        $row = $query->fetch();
+        dd($row);
+
+        $user = new \stdClass();
+        $user->name = "Marc";
+        $user->sn1 = "Calafell";
+        return view('home')
+            ->withUser($user);
+
     }
 }
