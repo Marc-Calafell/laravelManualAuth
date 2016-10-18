f188989f566c1e73435c481e511e6244cf52ca67
Marc <mcalafellsmx@gmail.com>
2016-10-18 20:59:04 +0200

    Factory Test

--- tests/login-test.php
+++ tests/login-test.php
@@ -7,12 +7,20 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class LoginTest extends TestCase
 {
+    use DatabaseMigrations;
 
-    public function testLoginPageShowsLoginForm()
-    {
+    public function testLoginPageShowsLoginForm() {
         $this->visit('/login')
             ->see('Usuari')
             ->see('Password')
         ;
     }
+
+    protected function createTestUser(){
+
+      return factory(App\User::class,1)->create();;
+
+    }
+
+
 }
