17cbfc8f7831d5b80e01129aa0f4586bad5ba339
Marc <mcalafellsmx@gmail.com>
2016-10-18 21:45:09 +0200

    redirect and catch exeptions

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\user;
 use App\Http\Requests;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class LoginController extends Controller
 {
@@ -15,7 +17,24 @@ class LoginController extends Controller
 
     public function login()
     {
-        return 'Holas';
+        try {
+            $user = User::where(['email' => $request->input('email')])->firstOrFail();
+
+        } catch (NotFoundHttpException $e){
+            return redirect('login');
+
+        }
+
+        if($user->pasword == Hash::make($request->input('password')) ) {
+            return redirect('home');
+
+        }else{
+            return redirect('login');
+
+        }
+
+
+        echo 'Holas';
     }
 
 }
--- tests/login-test.php
+++ tests/login-test.php
@@ -18,9 +18,17 @@ class LoginTest extends TestCase
 
     protected function createTestUser(){
 
-      return factory(App\User::class,1)->create();;
+      return factory(App\User::class,1)->create(['password'->]);
 
     }
 
+    public function testLoginPostWithUserOk() {
+        $user = $this->createTestUser();
+        $this->visit(/login)
+        ->type($user->name)
+            ->type('123456','password')
+            ->press('login')
+            ->seepage('/home')
+    }
 
 }
